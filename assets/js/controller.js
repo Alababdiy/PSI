66ffc3e47b796d4aca65c4c382c20459b9aba168
abdalmonem <abnhamoda@gmail.com>
2019-03-27 17:07:23 +0200

    home animations
    create animations plugin

---
+++ assets/js/controller.js
@@ -0,0 +1,36 @@
+function Duanimate(TheElement,action,animate,speed){
+    speed = speed;
+    if(action == "hide"){ action = "none" ;}
+    else if(action == "show"){ action = "inline" ;}
+    else if(action == "toggle"){
+        if( $(TheElement).is(":visible") ){
+            action = "none" ;
+            animate = animate.replace("In", "Out");
+        }else{
+            action = "inline";
+            animate = animate.replace("Out", "In");
+        }
+    }else if(action == "none"){
+        action = "inline" ;
+    }
+
+
+    speedOut = speed.split(".").join("");
+    if(speed.toString().indexOf('.') == "-1"){
+        speed = "0."+speed;
+        speedOut = Math.floor(speed)+100;
+    }
+
+
+
+    $(TheElement).addClass("animated "+animate).css("animation-duration",speed+"s");
+
+    if ($(TheElement).css('display') == 'none'){
+        $(TheElement).css("display",action); setTimeout(function () { $(TheElement).removeClass("animated "+animate);},speedOut);
+    }else{
+        $(TheElement).css("display",action); setTimeout(function () { $(TheElement).removeClass("animated "+animate);},speedOut);
+
+    }
+
+    return false;
+}
